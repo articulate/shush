1a0b19ebfe186cd379ad07314e7aed6adbe09ece
Luke van der Hoeven <hungerandthirst@gmail.com>
2015-01-29 17:24:31 -0500

    key expiration options

--- eyes_web.rb
+++ eyes_web.rb
@@ -3,13 +3,20 @@ require 'byebug' if ENV['RACK_ENV'] == 'development'
 
 require_relative 'secrest_store'
 
+TIMES = {
+  "1 week" => 10080,
+  "1 day" => 1440,
+  "1 hour" => 60,
+  "10 minutes" => 10,
+  "1 minute" => 1,
+}
+
 class EyesWeb < Sinatra::Base
-  include BCrypt
   def store
     @store ||= SecrestStore.new
   end
 
-  def key
+  def generate_key
     o = [('a'..'z'), ('A'..'Z')].map { |i| i.to_a }.flatten
     (0...50).map { o[rand(o.length)] }.join
   end
@@ -19,19 +26,34 @@ class EyesWeb < Sinatra::Base
   end
 
   post '/save' do
+    key = generate_key
     store.save key, params[:secret]
+    store.expire_in_minutes(key, params[:time]) if params[:expire] == 'time'
 
     # Generate url with key
     url = "http://localhost:9393/read/#{key}"
-
-    haml :share, locals: { url: url }
+    haml :share, locals: { url: url, time: TIMES.key(params[:time].to_i) }
   end
 
   get "/read/:key" do
-    note = store.fetch params[:key]
+    key = params[:key]
+    note = store.fetch key
+
+    return 404 unless note
+
+    if store.auto_expire?(key)
+      ttl = store.expires_in(key)
+    else
+      store.destroy key
+    end
 
     # Decrypt...
 
-    haml :note, locals: { note: note }
+    haml :note, locals: { note: note, ttl: ttl }
   end
+
+  not_found do
+    'That note does not exist!'
+  end
+
 end
--- secrest_store.rb
+++ secrest_store.rb
@@ -17,12 +17,25 @@ class SecrestStore
     redis.set(key, encrypt(value))
   end
 
-  def save_for_time(key, value, for_time:)
-    redis.save(key, encrypt(value))
-    redis.expire(key, for_time)
+  # Redis expire is in seconds, we're dealing in minutes
+  def expire_in_minutes(key, minutes)
+    redis.expire(key, (minutes.to_i * 60))
   end
 
   def fetch(key)
     redis.get(key)
+  end
+
+  def destroy(key)
+    redis.del key
+  end
+
+  # remember to convert to minutes coming back out to be consistent
+  def expires_in(key)
+    (redis.ttl(key).to_f / 60).round(2)
+  end
+
+  def auto_expire?(key)
+     expires_in(key) >= 0
   end
 end
