f776fdce66fdba74a1e6c88528a5315c2ee918b2
Luke van der Hoeven <hungerandthirst@gmail.com>
2015-01-31 14:17:19 -0500

    simple client-side note encryption

--- public/authoring.js
+++ public/authoring.js
@@ -1,13 +1,45 @@
 $(document).ready(function() {
-  $readSelect = $("#expire-read")
-  $timeSelect = $("#expire-time")
-  $times = $("#expire-at")
+  var $form = $('form#input')
+  var $save = $('#save')
+    , $message = $('#note')
+    , $readSelect = $("#expire-read")
+    , $timeSelect = $("#expire-time")
+    , $times = $("#expire-at");
 
   function toggleTimeSelect(evt) {
     $times.toggle($timeSelect.is(":checked"));
   }
 
+  function sendMessage(err, buff) {
+    if (!err) {
+      $message.val(buff.toString('hex'));
+      $form.submit();
+    } else {
+      $message.val("Could not secrest");
+    }
+  }
+
+  function progressReporter(obj) {
+    console.log(obj);
+  }
+
+  function encrypt(msg) {
+    triplesec.encrypt({
+      data:          new triplesec.Buffer(msg),
+      key:           new triplesec.Buffer('top-secret-pw'),
+      progress_hook: progressReporter
+    }, sendMessage);
+  }
+
   $readSelect.on("click", toggleTimeSelect);
   $timeSelect.on("click", toggleTimeSelect);
+
+  $save.on('click', function(evt) {
+    evt.preventDefault();
+    var msg = $message.val()
+
+    $message.val("Secresting...");
+    encrypt(msg);
+  });
 
 });
--- public/reading.js
+++ public/reading.js
@@ -2,11 +2,31 @@ $(document).ready(function() {
   $note = $("#note")
   $ttl = $("#ttl")
 
+  function decrypt(msg) {
+    triplesec.decrypt({
+      data: new triplesec.Buffer(msg, "hex"),
+      key:  new triplesec.Buffer('top-secret-pw'),
+      progress_hook: progressReporter
+    }, writeMessage);
+  }
+
+  function progressReporter(obj) {
+    console.log(obj);
+  }
+
+  function writeMessage(err, buff) {
+    if (!err) {
+      $note.text(buff.toString());
+    } else {
+      $note.text("Could not decrypt your secrest.")
+    }
+  }
+
   $.ajax("/note/" + $note.data('nid'), {contentType: "application/json"})
     .done(function(data) {
-      $note.text(data.note);
-      if(data.ttl) { $ttl.text("This note will be destroyed in " + data.ttl + " minutes."); }
+      decrypt(data.note);
 
+      if(data.ttl) { $ttl.text("This note will be destroyed in " + data.ttl + " minutes."); }
     }).fail(function(xhr, status, error) {
       window.location = "/read/not_found"
     });
