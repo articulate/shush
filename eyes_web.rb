65d1b75b0777f909e22994167acd55e6cede9d4c
Luke van der Hoeven <hungerandthirst@gmail.com>
2015-02-04 23:15:48 -0500

    flash messages

--- eyes_web.rb
+++ eyes_web.rb
@@ -1,6 +1,7 @@
 require 'json'
 require 'sinatra/base'
 require "sinatra/content_for"
+require 'rack-flash'
 require 'cryptor'
 require 'cryptor/symmetric_encryption/ciphers/xsalsa20poly1305'
 
@@ -19,6 +20,16 @@ require_relative "secrest_store"
 class EyesWeb < Sinatra::Base
   helpers Sinatra::ContentFor
   include ActionView::Helpers::DateHelper
+
+  FLASH_TYPES = %i[danger warning info success]
+
+  set :session_secret, ENV["SESSION_SECRET"]
+  use Rack::Session::Cookie, key:          "_rack_session",
+                             path:         "/",
+                             expire_after: 2592000, # In seconds
+                             secret:       settings.session_secret
+
+  use Rack::Flash, accessorize: FLASH_TYPES
 
   configure :development, :test do
     set :host, "articulatedev.com:9393"
