6534ac93e84b1ade82c48c7324620449d49b0fcc
Adam Ochonicki <aochonicki@articulate.com>
2015-02-26 12:24:00 -0500

    Revert "Email on read"

--- eyes_web.rb
+++ eyes_web.rb
@@ -9,16 +9,13 @@ require 'active_support/core_ext/numeric/time'
 require 'action_view'
 require 'action_view/helpers'
 
+require 'byebug' if ENV['RACK_ENV'] == 'development'
+
 if ENV["RACK_ENV"] == "production"
   require "rack/ssl-enforcer"
-  require 'postmark'
-else
-  require 'byebug'
-  require "letter_opener"
 end
 
 require_relative "secrest_store"
-require_relative "mail_notifier"
 
 class EyesWeb < Sinatra::Base
   helpers Sinatra::ContentFor
@@ -38,22 +35,16 @@ class EyesWeb < Sinatra::Base
     set :host, "articulatedev.com:9393"
     set :force_ssl, false
     set :redis_url, "redis://articulatedev.com:6379"
-    set :mailer, [LetterOpener::DeliveryMethod, location: File.expand_path('../tmp/letter_opener', __FILE__)]
   end
 
   configure :production do
     set :host, ENV["HOST"]
     set :force_ssl, true
     set :redis_url, ENV["REDISTOGO_URL"]
-    set :mailer, [Mail::Postmark, api_token: ENV['POSTMARK_API_TOKEN']]
   end
 
   set :redis, Redis.new(url: settings.redis_url)
   set :store, SecrestStore.new(settings.redis)
-
-  Mail.defaults do
-    delivery_method *EyesWeb.settings.mailer
-  end
 
   def store
     settings.store
@@ -82,10 +73,7 @@ class EyesWeb < Sinatra::Base
 
   post "/save" do
     time = timed?(params[:expire]) ? params[:time].to_i : nil
-    key = store.save params[:secret],
-      ttl: time,
-      notify: !params[:notify].nil?,
-      email: params[:notify_email]
+    key = store.save(params[:secret], ttl: time)
 
     # Generate url with key
     protocol = settings.force_ssl? ? "https" : "http"
@@ -109,8 +97,6 @@ class EyesWeb < Sinatra::Base
   get "/note/:fingerprint" do
     fingerprint = params[:fingerprint]
     data = store.fetch(fingerprint)
-
-    MailNotifier.notify_read(data[:email], fingerprint, is_ttl: data[:is_ttl]) if data[:request_notify]
 
     content_type :json
     {
