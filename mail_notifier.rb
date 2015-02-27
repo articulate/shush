fc61fec6d1c60b3cf1864548c45b196165ea5794
Luke van der Hoeven <hungerandthirst@gmail.com>
2015-02-27 15:18:17 -0500

    catch notify errors

--- mail_notifier.rb
+++ mail_notifier.rb
@@ -5,29 +5,33 @@ class MailNotifier
     DEFAULT_FROM = "Shush <plukevdh+shush@articulate.com>"
 
     def notify_read(email, link, is_ttl:)
-      message = Mail.new do
-        to email
-        from DEFAULT_FROM
-        subject "Shush: Message Read"
+      begin
+        message = Mail.new do
+          to email
+          from DEFAULT_FROM
+          subject "Shush: Message Read"
 
-        text_part do
-          msg = "Your message (#{link}) was read"
-          msg += is_ttl ? "." : "and destroyed."
+          text_part do
+            msg = "Your message (#{link}) was read"
+            msg += is_ttl ? "." : "and destroyed."
 
-          body msg
+            body msg
+          end
+
+          html_part do
+            msg = "<h2>Your message (<i>#{link}</i>) was read"
+            msg += is_ttl ? "." : " and destroyed."
+            msg += "</h2>"
+
+            content_type 'text/html; charset=UTF-8'
+            body msg
+          end
         end
 
-        html_part do
-          msg = "<h2>Your message (<i>#{link}</i>) was read"
-          msg += is_ttl ? "." : " and destroyed."
-          msg += "</h2>"
-
-          content_type 'text/html; charset=UTF-8'
-          body msg
-        end
+        message.deliver
+      rescue => e
+        puts "Could not send notification: #{e.message}"
       end
-
-      message.deliver
     end
   end
 end
