2b1e5591da24392130f70cadda48b6ab23c31777
Luke van der Hoeven <hungerandthirst@gmail.com>
2015-09-03 15:09:29 -0400

    secrest easter egg

--- services/secrest_imager.rb
+++ services/secrest_imager.rb
@@ -3,16 +3,22 @@ require 'http'
 class SecrestImager
   BASE_URL = 'https://ajax.googleapis.com/ajax/services/search/images'
   BASE_SEARCH = "ryan secrest"
+  PAGE_SIZE = 8
+
+  def initialize
+    @rnd = Random.new
+  end
 
   # returns array size of <count> random image urls
   def get(count=1)
     images = fetch_images
 
     1.upto(count.to_i).map do |i|
-      index = Random.new.rand(images.count)
-      images[index]
+      random(images)
     end
   end
+
+  private
 
   def fetch_images
     response = HTTP.accept(:json).get(BASE_URL, params: request_params)
@@ -31,9 +37,15 @@ class SecrestImager
     {
       q: BASE_SEARCH,
       safe: 'active',
+      rsz: 8,
+      start: (@rnd.rand(8) * 8),
       v: '1.0'
     }
   end
 
+  def random(images)
+    index = @rnd.rand(images.count)
+    images[index]
+  end
 end
 
